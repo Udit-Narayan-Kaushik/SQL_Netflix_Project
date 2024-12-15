bec1cae359d1e39e599ad96dc23db8024c064329
Udit_Narayan_Kaushik <155947564+Udit-Narayan-Kaushik@users.noreply.github.com>
2024-12-15 14:00:40 +0530

    Add files via upload

---
+++ SQL_Schema.sql
@@ -0,0 +1,18 @@
+ DROP TABLE IF EXISTS netflix;
+
+CREATE TABLE netflix (
+
+show_id VARCHAR(50),
+sh_type VARCHAR(200),
+title VARCHAR(1500),
+director VARCHAR(208),
+casts VARCHAR(1050),
+country VARCHAR(250),
+date_added VARCHAR(500),
+release_year  INT,
+rating VARCHAR(200),
+duration VARCHAR(200),
+listed_in VARCHAR(300),
+description VARCHAR(320)
+
+);
